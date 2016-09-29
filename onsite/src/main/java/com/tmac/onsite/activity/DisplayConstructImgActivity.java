676444c695708971d4976ee51bd197476c103f0f
linsentmac <tmacyouxiaoke@163.com>
2016-09-29 20:04:14 +0800

    Add ViewPagerIndicator Library
    Signed-off-by: linsentmac <tmacyouxiaoke@163.com>

---
+++ onsite/src/main/java/com/tmac/onsite/activity/DisplayConstructImgActivity.java
@@ -0,0 +1,13 @@
+package com.tmac.onsite.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisplayConstructImgActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_construct_img);
+    }
+}
